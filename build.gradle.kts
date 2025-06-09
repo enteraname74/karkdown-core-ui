03a402afed32b3457ebe24da7c4efb8be67a2acc
enteraname74 <noahpenin@gmail.com>
2025-06-09 13:57:31 +0200

    Add basic project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    alias(libs.plugins.multiplatform)
-    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
 }
 
-description = "Core UI elements of the app"
-group = "com.github.enteraname74.karkdowncoreui"
+kotlin {
+    jvmToolchain(17)
+}
+
 version = "0.1.0"
 
 repositories {
@@ -13,20 +15,19 @@ repositories {
     mavenCentral()
 }
 
-
 kotlin {
-    jvmToolchain(17)
     jvm()
 
     sourceSets {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
+            implementation(compose.material)
             implementation(compose.material3)
             implementation(compose.materialIconsExtended)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.kotlinx.coroutines.core)
+            implementation(compose.ui)
+
+            implementation(files("karkdown-core-jvm-0.1.0.jar"))
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,32 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-rootProject.name = "karkdown-core-ui"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "karkdown-core-ui"
